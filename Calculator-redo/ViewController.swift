88cd2e65ac2a60e7fc3acc1b3177959adf95f69d
xenia0 <xenia.chiru@gmail.com>
2016-09-06 22:14:23 -0700

    Digit buttons for calculator

--- Calculator-redo/ViewController.swift
+++ Calculator-redo/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculatorName: UILabel!
+    
+    @IBOutlet weak var answerOutput: UILabel!
+    
+    @IBAction func digitButton(sender: UIButton) {
+        answerOutput.text = answerOutput.text! + sender.currentTitle!
+    }
+    
+    @IBAction func clearAnswerOutput(sender: UIButton) {
+        answerOutput.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
